c85197fa2478518635c1ea590c24d992848955a4
vinayreddygujjula <vinayreddyg02@gmail.com>
2023-11-01 23:13:05 -0500

    choose end point works

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var ticketsRouter = require('./routes/tickets');
 var boardRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/tickets', ticketsRouter);
 app.use('/board', boardRouter);
+app.use('/choose', chooseRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
