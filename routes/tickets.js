0efdd474aefef557022fa88b599cafb6f55c0b20
vinayreddygujjula <vinayreddyg02@gmail.com>
2023-11-01 22:50:39 -0500

    modifies index and ticket

--- routes/tickets.js
+++ routes/tickets.js
@@ -1,9 +1,47 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('tickets', { title: 'Search Results - TICKETS' });
-});
+const results = [
+    { 
+        eventName: 'Singing', 
+        venue: 'Colden Hall',
+        price: 100,
+        location: 'Maryville',
+        ticketType: 'General' 
+    },
 
-module.exports = router;
+    { 
+        eventName: 'Dancing',
+        venue: 'valk building',
+        price: 200, 
+        location: 'Maryville',
+        ticketType: 'VIP' 
+    },
+    { 
+        eventName: 'Movie',
+        venue: 'Hall', 
+        price: 150, 
+        location: 'Kansas', 
+        ticketType: 'Standard' 
+    },
+    { 
+        eventName: 'ABC Consert',
+        venue: 'Amb', 
+        price: 3000, 
+        location: 'Omaha', 
+        ticketType: 'VIP' 
+    },
+    { 
+        eventName: 'Sprots',
+        venue: 'huges field', 
+        price: 2000, 
+        location: 'Maryville', 
+        ticketType: 'General' 
+    },
+];
+
+router.get('/', (req, res) => {   
+    res.render('tickets', { title: 'Search Results - TICKETS', r: results });
+  });
+  
+  module.exports = router;
